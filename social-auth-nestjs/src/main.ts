174ced3cd39293e26a504fd23f5d5eb6ee6abc89
orlein <pcj9024@gmail.com>
2024-02-23 17:28:05 +0900

    add cors

--- social-auth-nestjs/src/main.ts
+++ social-auth-nestjs/src/main.ts
@@ -6,6 +6,15 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors({
+    origin: [
+      'http://localhost:3000',
+      'http://localhost:3001',
+      'https://social-auth-nextjs.vercel.app/',
+    ],
+    credentials: true,
+  });
+
   app.useGlobalPipes(new ZodValidationPipe());
 
   const config = new DocumentBuilder()
